ec73170e700455692af9eec299e975764ceeaf9b
JaiKV <mazimzaidan@gmail.com>
2024-11-17 15:48:24 +0800

    Initial commit

---
+++ app/src/main/java/com/azim/lab05/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.azim.lab05
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.azim.lab05.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding     = ActivityMainBinding.inflate(layoutInflater)
+        val view    = binding.root
+        setContentView(view)
+    }
+}
