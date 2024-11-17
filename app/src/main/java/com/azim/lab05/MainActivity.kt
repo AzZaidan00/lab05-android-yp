1ecde01c9425a16402e83f9bf043e3a23acb2e23
JaiKV <mazimzaidan@gmail.com>
2024-11-17 16:04:16 +0800

    Updated

--- app/src/main/java/com/azim/lab05/MainActivity.kt
+++ app/src/main/java/com/azim/lab05/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.azim.lab05.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    // ActivityMain => Reverse of our Activity name (MainActivity)
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +17,31 @@ class MainActivity : AppCompatActivity() {
         binding     = ActivityMainBinding.inflate(layoutInflater)
         val view    = binding.root
         setContentView(view)
+
+        binding.btnOrder.setOnClickListener {
+            var pizzaSizePrice = 0.0    // type Double
+            var toppingSize = 0.0       // type Double
+
+            // Only 1 option can be selected
+            when {
+                // isChecked is the property to see if an item is selected
+                binding.rdBtnSmall.isChecked    -> pizzaSizePrice = 5.00
+                binding.rdBtnMedium.isChecked   -> pizzaSizePrice = 7.00
+                binding.rdBtnLarge.isChecked    -> pizzaSizePrice = 10.00
+            }
+
+            // Multiple choices can be selected
+            if (binding.boxOlives.isChecked) {
+                toppingSize += 2
+            }
+            if (binding.boxOnions.isChecked) {
+                toppingSize += 1
+            }
+            if (binding.boxTomatoes.isChecked) {
+                toppingSize += 3
+            }
+
+            binding.txtResult.text = "Total Price is : RM ${pizzaSizePrice + toppingSize}"
+        }
     }
 }
